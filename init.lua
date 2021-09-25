9be7f32e7d6149c8084553dce91fd6c9906ff5ba
NO11 <68117246+NO411@users.noreply.github.com>
2021-09-25 15:00:53 -0500

    Add random mob drops

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 extreme_randomizer = {
         items = {},
         random_craft = {},
+	drops = {},
+	random_drops = {},
 }
 
 minetest.register_on_mods_loaded(function()
@@ -17,6 +19,23 @@ minetest.register_on_mods_loaded(function()
                         drop = extreme_randomizer.items[math.random(#extreme_randomizer.items)],
                 })
         end
+	for entity, def in pairs(minetest.registered_entities) do
+		local drops = def.drops
+		if drops then
+			table.insert(extreme_randomizer.drops, drops)
+		end
+	end
+	for entity, def in pairs(minetest.registered_entities) do
+		local drops = def.drops
+		if drops then
+			extreme_randomizer.random_drops[entity] = extreme_randomizer.drops[math.random(#extreme_randomizer.drops)]
+		end
+	end
+	for entity, def in pairs(minetest.registered_entities) do
+		if def.drops then
+			def.drops = extreme_randomizer.random_drops[entity]
+		end
+	end
 end)
 
 for _, craft in pairs({ "craft_predict", "on_craft" }) do
